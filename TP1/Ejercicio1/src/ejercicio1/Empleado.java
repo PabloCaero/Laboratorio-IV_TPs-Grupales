1dc014f02b4c9ccb0d938bc2fadd516a0b8b6d95
Pablo Ezequiel Caero <pablo_ieme@hotmail.com>
2022-03-26 10:59:08 -0300

    Proyecto reordenado

--- TP1/Ejercicio1/src/ejercicio1/Empleado.java
+++ TP1/Ejercicio1/src/ejercicio1/Empleado.java
@@ -1,61 +1,68 @@
 package ejercicio1;
 
+//(PUNTO 2)
 public class Empleado {
 
-	//ATRIBUTOS - CONCEPTO DE ENCAPSULACIÓN
-		private int id; // EL FINAL EVITA QUE SE MODIFIQUE EL ID MANUALMENTE
-		private String nombre;
-		private int edad;
-	    private  static int cont =999;// INCREMENTA DE A 1 EN 1  LOS ID PARA GENERAR EL ID AUTOMATICAMENTE
-		
-	//CONSTRUCTORES
-		public Empleado()
-		{
-	    cont++;
-		this.id= cont;
-		this.nombre ="Sin nombre";
-		this.edad = 99 ;
-		
-		}
-		
-		public Empleado(String nombre,int edad) 
-		{
-			cont++;
-			this.id=   cont;
-			this.nombre =nombre;
-			this.edad = edad;
+	//ATRIBUTOS - CONCEPTO DE ENCAPSULACIÓN (PUNTO 1)
+			private final int id; // EL FINAL EVITA QUE SE MODIFIQUE EL ID MANUALMENTE
+			private String nombre;
+			private int edad;
+		    private  static int cont =999;// VARIABLE QUE PERMITE QUE LOS  ID ARRANQUEN  EN 1000
 			
-		}
+		//CONSTRUCTORES
+		    
+		//(PUNTO 5)
+			public Empleado()
+			{
+			//(PUNTO 4)
+		    cont++;//INCREMENTO DE A 1 EN 1  LOS ID AUTOMATICAMENTE
+		    this.id= cont; 
+		    
+			this.nombre ="Sin nombre"; 
+			this.edad = 99 ; 			
+			}
+			
+		//(PUNTO 5)
+			public Empleado(String nombre,int edad) 
+			{
+				cont++;
+				this.id=   cont;
+				this.nombre = nombre;
+				this.edad = edad;
+				
+			}
+			
+		//GETTERS Y SETTERS
+			public int getId() {
+				return id;
+			}
+			
+			/*public void setId(int id) {
+				this.id = id;
+			}*/
+			
+			public String getNombre() {
+				return nombre;
+			}
+			public void setNombre(String nombre) {
+				this.nombre = nombre;
+			}
+			public int getEdad() {
+				return edad;
+			}
+			public void setEdad(int edad) {
+				this.edad = edad;
+			}
+
 		
-	//GETTERS Y SETTERS
-		public int getId() {
-			return id;
-		}
-		public void setId(int id) {
-			this.id = id;
-		}
-		public String getNombre() {
-			return nombre;
-		}
-		public void setNombre(String nombre) {
-			this.nombre = nombre;
-		}
-		public int getEdad() {
-			return edad;
-		}
-		public void setEdad(int edad) {
-			this.edad = edad;
-		}
-		
-	// Punto 6
-		
+	//(PUNTO 6)		
 		public static String devuelveProximoID() 
 		{
 			int aux = cont+1;
 			return "El proximo empleado: " + aux;
 		}
 		
-	//METODO toString()	
+	//METODO toString()	(PUNTO 7)
 		public String toString() {
 			return "ID = " + id + ", NOMBRE = " + nombre + ", EDAD = " + edad ;
 		}
